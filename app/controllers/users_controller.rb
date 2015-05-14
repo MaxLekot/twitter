9ebde5cfe4adff514ba249da6c2258daeac6d513
Max Lekot <kotvomne@ya.ru>
2015-05-14 21:35:00 +0400

    It work right

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
    end
    
    def show
-     @user = User.find(params[:id])
-   end
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
    
   def new
      if signed_in?
@@ -63,12 +64,7 @@ class UsersController < ApplicationController
    
    # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    
     
     def correct_user
       @user = User.find(params[:id])
